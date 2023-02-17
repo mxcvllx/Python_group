9931d1ac4b52bac79b621d04b85bcf16aca18a4a
_Odil <122612295+Odiljon11@users.noreply.github.com>
2023-02-17 11:14:37 +0500

    HOMEWORK

--- BOTS/Weather_APP_bot/main.py
+++ BOTS/Weather_APP_bot/main.py
@@ -1,50 +1,46 @@
-from datetime import datetime
+import requests
+from BOTS.Weather_APP_bot.config import BOT_TOKEN, wether_api
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 import telebot
-from environs import Env
 from telebot.types import BotCommand
 
-from get_data import get_data
-from BOTS.Weather_APP_bot.keybosrds import days_inline_btn
-from BOTS.Weather_APP_bot.keybosrds import weather_inline_btn
-
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env("BOT_TOKEN")
-
-bot = telebot.TeleBot(BOT_TOKEN, parse_mode="html")
+bot = Bot(token=BOT_TOKEN)
+bott = telebot.TeleBot(BOT_TOKEN, parse_mode="html")
+dp = Dispatcher(bot)
 
 
-# /weather
-@bot.message_handler(commands=["weather"])
-def weather_handler(message):
-    today = datetime.now()
-    weather_data = get_data()
-    today_weather = None
-    for day_weather in weather_data:
-        day_date = datetime.strptime(day_weather.get("day"), "%Y.%m.%d")
-        if day_date.date() == today.date():
-            today_weather = day_weather
-    msg = f"сегоднешняя погода ->:\n\n" \
-          f"градус ->{today_weather.get('Средняя тепература')}"
-    bot.send_message(message.chat.id, msg, parse_mode="html", reply_markup=days_inline_btn)
+# /start....
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    user_name = message.from_user
+    await message.reply(f"Привет {user_name.first_name}, введите название города ")
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def weather_handler(call):
-    if call.data == "5":
-        bot.send_message(call.message.chat.id, "Entry date ", reply_markup=weather_inline_btn)
-    if call.data != "5":
-        bot.send_message(call.message.chat.id, f"{call.data}")
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q=" + message.text + "&appid=" + wether_api + "&units=metric"
+        )
+        data = r.json()
+
+        city_name = data["name"]
+        weather = data["main"]["temp"]
+
+        await message.reply(f"Погода в городе -->: {city_name}\nТемпература: {weather}C°")
+    except:
+        await message.reply("Проверте название города")
 
 
 def my_commands():
     return [
-        BotCommand("/weather", "Сегоднящная погода")
+        BotCommand("/start", "Start bot")
     ]
 
 
 if __name__ == "__main__":
     print("Started...")
-    bot.set_my_commands(commands=my_commands())
-    bot.infinity_polling()
+    bott.set_my_commands(commands=my_commands())
+    executor.start_polling(dp)
